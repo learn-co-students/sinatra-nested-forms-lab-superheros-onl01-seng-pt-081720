b32b854b3470964691ca61c82d4dcc2b609d12e1
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-28 21:38:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
  set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :create
+    erb :super_hero
+  end
+
+  post '/team' do
+    @par = params[:team]
+    erb :team
   end
 
 
